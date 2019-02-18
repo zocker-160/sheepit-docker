a18ba062dc876829fa0793cf0e21b7ad08ecd173
Unknown <zocker1600@posteo.net>
2019-02-18 15:12:32 +0100

    change client mode to text

--- startapp.sh
+++ startapp.sh
@@ -20,7 +20,7 @@ if [ -z $gpu ]; then
 	java -jar /sheep/sheepit-client.jar --show-gpu
 	echo exiting....
 else
-	java -jar /sheep/sheepit-client.jar -cache-dir /sheep/cache -cores $cpu -compute-method GPU -gpu $gpu -login $user_name -password $user_password -ui oneLine
+	java -jar /sheep/sheepit-client.jar -cache-dir /sheep/cache -cores $cpu -compute-method GPU -gpu $gpu -login $user_name -password $user_password -ui text
 fi
 
 #java -jar /sheep/sheepit-client.jar -cores $cpu 
