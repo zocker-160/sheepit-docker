53929f79aac8d0b095ec3e73588dd0f3c152dfaf
zocker-160 <zocker1600@posteo.net>
2022-07-23 18:22:25 +0200

    second test for vast.ai image

--- startapp_vast.sh
+++ startapp_vast.sh
@@ -12,17 +12,5 @@ curl https://www.sheepit-renderfarm.com/media/applet/client-latest.php -o /sheep
 cpu=`nproc`
 echo "No core count specified, autodetected $cpu cores."
 
-if [ -z $gpu ]; then
-    echo "no GPU_ID specified, showing all supported GPUs:"
-    java -jar /sheep/sheepit-client.jar --show-gpu
-    
-    echo "exiting...."
-else
-    if [[ $gpu == "none" ]]; then
-        echo "running in CPU mode"
-        java -jar /sheep/sheepit-client.jar -cache-dir /sheep/cache -cores $cpu -login $user_name -password $user_password -ui $ui "$@"
-    else
-        echo "running in GPU mode"
-        java -jar /sheep/sheepit-client.jar -cache-dir /sheep/cache -cores $cpu -compute-method GPU -gpu $gpu -login $user_name -password $user_password -ui $ui "$@"
-    fi
-fi
+echo "Starting client"
+java -jar /sheep/sheepit-client.jar -cache-dir /sheep/cache -cores $cpu -compute-method GPU "$@"
