7e6c3f8025742ef340d0f04010faa14bde74540f
zocker-160 <zocker1600@posteo.net>
2023-09-27 16:31:51 +0200

    fix missing Blender dependencies

--- startapp_vast.sh
+++ startapp_vast.sh
@@ -10,7 +10,7 @@ curl https://www.sheepit-renderfarm.com/media/applet/client-latest.php -o /sheep
 
 # Autodetect cores
 cpu=`nproc`
-echo "No core count specified, autodetected $cpu cores."
+echo "Autodetected $cpu cores."
 
 echo "Starting client"
 java -jar /sheep/sheepit-client.jar -cache-dir /sheep/cache -cores $cpu -compute-method GPU "$@"
